44de9979415be84c9fedb781f5368f7c285ff698
daniel <danielncg1@gmail.com>
2024-06-18 17:28:53 -0400

    Variables' names changed.

--- model.py
+++ model.py
@@ -18,8 +18,8 @@ class Model:
         self.l2 = float(params["Systemic inertance (mmHg s2 cm3)"])
         self.l3 = float(params["Pulmonary artery inertance (mmHg s2 cm3)"])
         self.l4 = float(params["Pulmonary inertance (mmHg s2 cm3)"])
-        self.pipl = float(params["Left peak isovolumic pressure (mmHg)"])
-        self.pipr = float(params["Right peak isovolumetric pressure (mmHg)"])
+        self.p0l = float(params["Left peak isovolumic pressure (mmHg)"])
+        self.p0r = float(params["Right peak isovolumetric pressure (mmHg)"])
         self.r1 = float(params["Aortic valve resistance (mmHg s cm3)"])
         self.r2 = float(params["Aortic-systemic resistance (mmHg s cm3)"])
         self.r3 = float(params["Systemic resistance (mmHg s cm3)"])
@@ -28,8 +28,8 @@ class Model:
         self.r6 = float(params["Pulmonary artery resistance (mmHg s cm3)"])
         self.r7 = float(params["Pulmonary resistance (mmHg s cm3)"])
         self.r8 = float(params["Mitral valve resistance (mmHg s cm3)"])
-        self.rml = float(params["Left myo. viscosity resistance (mmHg s cm3)"])
-        self.rmr = float(params["Right myo. viscosity resistance (mmHg s cm3)"])
+        self.rl = float(params["Left myo. viscosity resistance (mmHg s cm3)"])
+        self.rr = float(params["Right myo. viscosity resistance (mmHg s cm3)"])
         self.tc = float(params["Cardiac period (s)"])
         self.ti = float(init_cond["Simulation initial instant (s)"])
         self.tf = float(init_cond["Simulation final instant (s)"])
@@ -59,12 +59,12 @@ class Model:
         return a
 
     def pressure_right_ventricle(self, t):
-        pir = self.pipr * self.activation(t)
-        return pir
+        pr = self.p0r * self.activation(t)
+        return pr
 
     def pressure_left_ventricle(self, t):
-        pil = self.pipl * self.activation(t)
-        return pil
+        pl = self.p0l * self.activation(t)
+        return pl
 
     def elastance_right_ventricle(self, t):
         er = self.ed_r + self.es_r * self.activation(t)
@@ -85,17 +85,17 @@ class Model:
         s3 = delta_p3 > 0
         s4 = delta_p4 > 0
 
-        dx1_dt = (1 / self.c1) * (s1 * delta_p1 / (self.rml + self.r1) - x2)
+        dx1_dt = (1 / self.c1) * (s1 * delta_p1 / (self.rl + self.r1) - x2)
         dx2_dt = (1 / self.l1) * (x1 - self.r2 * x2 - x3)
         dx3_dt = (1 / self.c2) * (x2 - x4)
         dx4_dt = (1 / self.l2) * (x3 - self.r3 * x4 - x5)
         dx5_dt = (1 / self.c3) * (x4 - (s2 * delta_p2 / self.r4))
-        dx6_dt = (s2 * delta_p2 / self.r4) - ((s3 * delta_p3) / (self.rmr + self.r5))
-        dx7_dt = (1 / self.c4) * (((s3 * delta_p3) / (self.rmr + self.r5)) - x8)
+        dx6_dt = (s2 * delta_p2 / self.r4) - ((s3 * delta_p3) / (self.rr + self.r5))
+        dx7_dt = (1 / self.c4) * (((s3 * delta_p3) / (self.rr + self.r5)) - x8)
         dx8_dt = (1 / self.l3) * (x7 - self.r6 * x8 - x9)
         dx9_dt = (1 / self.c5) * (x8 - x10)
         dx10_dt = (1 / self.l4) * (x9 - self.r7 * x10 - x11)
         dx11_dt = (1 / self.c6) * (x10 - (s4 * delta_p4 / self.r8))
-        dx12_dt = (s4 * delta_p4 / self.r8) - ((s1 * delta_p1) / (self.r1 + self.rml))
+        dx12_dt = (s4 * delta_p4 / self.r8) - ((s1 * delta_p1) / (self.r1 + self.rl))
 
         return [dx1_dt, dx2_dt, dx3_dt, dx4_dt, dx5_dt, dx6_dt, dx7_dt, dx8_dt, dx9_dt, dx10_dt, dx11_dt, dx12_dt]
